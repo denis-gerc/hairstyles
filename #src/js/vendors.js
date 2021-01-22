0fbfebe07f10084b3edb06b52029f8c4a47461ea
denis-gerc <gdv1001@gmail.com>
2021-01-22 15:26:50 +0300

    completed section certificates

--- #src/js/vendors.js
+++ #src/js/vendors.js
@@ -1,4 +1,5 @@
-@@include('libs/smoothScroll.js', {})
+// @@include('libs/smoothScroll.js', {})
 @@include('libs/swiper.min.js', {})
 @@include('libs/jquery.event.move.js', {})
 @@include('libs/jquery.twentytwenty.js', {})
+@@include('libs/lightgallery.min.js', {})
